22f4f72adbfe9a626b66e4da04557ec3ccc61a7d
Lance <Metal_Death_Music@yahoo.com>
2021-06-02 16:45:18 -0400

    Init commit zombie hunter app

---
+++ ZombieHunterApp/src/ZombieHunterApp.java
@@ -0,0 +1,29 @@
+
+public class ZombieHunterApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Zombie Hunter app!!!");
+		
+		String backpack[] = {"Shotgun", "Assault Rifle", "Sniper Rifle"};
+		String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+		int numbersZombiesHate[] = {4, 90, 70, 123, 12, 4, 561, 1};
+		
+		System.out.println("Backpack items!");
+		System.out.println(backpack[0]);
+		System.out.println(backpack[1]);
+		System.out.println(backpack[2]);
+		
+		System.out.println("These are the zombies!");
+		System.out.println(zombies[0]);
+		System.out.println(zombies[1]);
+		System.out.println(zombies[2]);
+
+		System.out.println(backpack[2]);
+		System.out.println(backpack[2]);
+		System.out.println(backpack[2]);
+		System.out.println(backpack[0]);
+		
+		System.out.println(numbersZombiesHate[6]);
+	}
+
+}
