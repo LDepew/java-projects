5df9a86c63127c07071952a48b5af61890f7e149
Lance <Metal_Death_Music@yahoo.com>
2023-03-29 14:58:41 -0400

    Banking App update

--- BankingApp/src/BankingApp.java
+++ BankingApp/src/BankingApp.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
 
 public class BankingApp {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter account name: ");
+		String name = sc.next();
+		
+		if (name.equals("bob")) {
 		Account bob = new Account("Bob Bank", "00001");
 		bob.showMenu();
-
+		}
+		else if (name.equals("jane")) {
+		Account jane = new Account("Jane Bank", "00002");
+		jane.showMenu();
+		}
+		sc.close();
 	}
 
 }
